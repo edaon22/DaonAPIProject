02b6491474bee764ed679201f67f28fe22ec6517
edaon22 <edaon22@sjnd.org>
2022-03-25 14:20:36 -0700

    3/25 image

--- src/AxolAPI_Response.java
+++ src/AxolAPI_Response.java
@@ -2,45 +2,66 @@ import java.net.URL;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.File;
 public class AxolAPI_Response {
 
     private static final String APIURL = "https://axoltlapi.herokuapp.com/";
     private String imageURL;
     private String fact;
-/*
-    public AxolAPI_Response(String imageURL, String fact){
-        this.imageURL = imageURL;
-        this.fact = fact;
-    }
-*/
+
+    public String getImageURL(){ return imageURL;}
+
+    public String getFact(){ return fact;}
+
     public boolean getData() {
-    try {
-        URL url = new URL(APIURL);
-        BufferedReader in = new BufferedReader(
+        try {
+            URL url = new URL(APIURL);
+            BufferedReader in = new BufferedReader(
                 new InputStreamReader(url.openStream()));
-
-        JSONParser parser = new JSONParser();
-        JSONObject axolData = (JSONObject) parser.parse(in);
-
-        fact = (String) axolData.get("facts");
-        imageURL = (String) axolData.get("url");
-
-        return true;
-    }catch(Exception e) {
+            JSONParser parser = new JSONParser();
+            JSONObject axolData = (JSONObject) parser.parse(in);
+            fact = (String) axolData.get("facts");
+            imageURL = (String) axolData.get("url");
+            return true;
+        }
+        catch(Exception e) {
         return false;
+        }
     }
+    /*
+    AxolAPI_Response dataRequest = new AxolAPI_Response();
+
+    String imageUrl = dataRequest.getImageURL();
+    String destinationFile = "image.jpg";
+    */
+
+    public static void saveImage(String imageUrl, String destinationFile) throws IOException {
+        URL url = new URL(imageUrl);
+        InputStream is = url.openStream();
+        OutputStream os = new FileOutputStream(destinationFile);
+
+        byte[] b = new byte[2048];
+        int length;
+
+        while ((length = is.read(b)) != -1) {
+            os.write(b, 0, length);
+        }
+
+        is.close();
+        os.close();
     }
-
-
-
-
-
-    public String getImageURL(){
-        return imageURL;
-    }
-
-    public String getFact(){
-        return fact;
+    public void cleanUp(String destinationFile){
+        File image = new File(destinationFile);
+            if (image.delete()) {
+                System.out.println("Deleted the image file.");
+            }
+            else {
+                System.out.println("Failed to delete the image file.");
+            }
     }
 
 
--- src/AxolFacts.java
+++ src/AxolFacts.java
@@ -7,55 +7,58 @@ public class AxolFacts {
 
         boolean keepUsing = true;
         AxolAPI_Response dataRequest = new AxolAPI_Response();
+        String destinationFile = "axolImage.jpg";
 
         while (keepUsing) {
             String mainResponse = ui.mainMenuPrompt();
-            if (mainResponse.equals("1")){ //yes image
+            if (mainResponse.equals("1")) { //yes image
                 int numFacts = ui.numFactsPrompt();
-                if (numFacts == 1){
+                if (numFacts == 1) {
                     System.out.println("Here is your image and fact:");
-                }
-                else if (numFacts > 1 && numFacts <= 5){
+                } else if (numFacts > 1 && numFacts <= 5) {
                     System.out.println("Here is your image and " + numFacts + " facts:");
                 }
-                if (dataRequest.getData()){
-                    for (int i = 0; i < numFacts; i++){
-                        dataRequest.getData();
-                        System.out.println((i+1) + ". " + dataRequest.getFact());
+                for (int i = 0; i < numFacts; i++) {  //Check for blank facts
+                    if (dataRequest.getData()) {
+                        System.out.println((i + 1) + ". " + dataRequest.getFact());
                     }
-                    System.out.println("URL: " + dataRequest.getImageURL());
                 }
+                System.out.println("URL: " + dataRequest.getImageURL());
+                //here
+                dataRequest.saveImage(dataRequest.getImageURL(), destinationFile);
+
 
                 String useAgainResponse = ui.useAgainPrompt();
-                if (useAgainResponse.equals("2")){
+                if (useAgainResponse.equals("2")) {
                     System.out.println(ui.goodbye());
+                    //dataRequest.cleanUp(destinationFile);
                     keepUsing = false;
                 }
-            }
-            else if (mainResponse.equals("2")){ //no image
+
+            } else if (mainResponse.equals("2")) { //no image
                 int numFacts = ui.numFactsPrompt();
-                if (numFacts == 1){
+                if (numFacts == 1) {
                     System.out.println("Here is your fact:");
-                }
-                else if (numFacts > 1 && numFacts <= 5){
+                } else if (numFacts > 1 && numFacts <= 5) {
                     System.out.println("Here are your " + numFacts + " facts:");
                 }
-                if (dataRequest.getData()){
-                    for (int i = 0; i < numFacts; i++){
+                if (dataRequest.getData()) {
+                    for (int i = 0; i < numFacts; i++) {
                         dataRequest.getData();
-                        System.out.println((i+1) + ". " + dataRequest.getFact());
+                        System.out.println((i + 1) + ". " + dataRequest.getFact());
                     }
                 }
                 String useAgainResponse = ui.useAgainPrompt();
-                if (useAgainResponse.equals("2")){
+                if (useAgainResponse.equals("2")) {
                     System.out.println(ui.goodbye());
                     keepUsing = false;
                 }
-            }
-            else if (mainResponse.equals("3")){
+
+            } else if (mainResponse.equals("3")) {
                 System.out.println(ui.goodbye());
                 break;
             }
+
         }
     }
 }
